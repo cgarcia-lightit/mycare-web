21556000806250fbe2ff178c42c27c94598980e3
Carlos Garcia <carlosgarcia@Carloss-MacBook-Pro-2.local>
2022-11-11 16:10:20 -0300

    feat: add layouts base for components

---
+++ components/layout/form/InputBase.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <label class="text-gray-500 text-sm leading-5 font-normal" :for="id" @show="label !== null">{{ label }}</label>
+    <input
+      :id="id"
+      :name="id"
+      :type="type"
+      autocomplete="current-password"
+      class="relative block w-full appearance-none  rounded-b-md border border-gray-300 px-3 py-2
+             text-gray-900 placeholder-gray-500 hover:border-gray-500 focus:z-10 focus:border-blue-500
+             focus:outline-none focus:ring-indigo-500 sm:text-sm"
+      @input="$emit('update:value', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputBase',
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: null
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    value: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/layout/tables/TableBase.vue
@@ -0,0 +1,59 @@
+<template>
+  <table class="table-auto w-3/4 border rounded-lg">
+    <thead>
+      <tr class="bg-gray-50 h-12 text-gray-500  mx-2 text-xs leading-4 font-medium uppercase tracking-wider">
+        <slot name="headers"/>
+      </tr>
+    </thead>
+    <tbody>
+      <slot name="body"/>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TableBase'
+}
+</script>
+
+<style scoped>
+
+tbody tr {
+  color: #111827;
+  height: 50px;
+}
+
+tbody tr:nth-child(even) {
+  background-color: #F9FAFB;
+}
+
+tbody tr td {
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+
+table {
+  margin-left: 120px;
+  border-spacing: 0;
+  border-collapse: separate;
+}
+table tbody tr:last-child td:last-child {
+  border-bottom-right-radius:0.5rem;
+}
+
+table tbody tr:last-child td:first-child {
+  border-bottom-left-radius:0.5rem;
+}
+
+table thead tr:first-child th:first-child,
+  table thead tr:first-child td:first-child {
+  border-top-left-radius:0.5rem
+}
+
+table tr:first-child th:last-child,
+  table thead tr:first-child td:last-child {
+  border-top-right-radius:0.5rem
+}
+
+</style>
